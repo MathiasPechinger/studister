cb54f23b5993c787702f19a362df3f8fb41464c9
arminstr <armin.straller@gmail.com>
2021-09-30 09:26:39 +0200

    added firebase config

--- fireBase.js
+++ fireBase.js
@@ -1,9 +1,20 @@
 var app_fireBase = {};
 (function(){
+  // Import the functions you need from the SDKs you need
+  // import { initializeApp } from "firebase/app";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
   // Your web app's Firebase configuration
-  var config = {
-    // your api config
+  const config = {
+    apiKey: "AIzaSyDRNEQbVnhlmbimWlWkmOi_WcW-EP1Bk8c",
+    authDomain: "studister-4b944.firebaseapp.com",
+    projectId: "studister-4b944",
+    storageBucket: "studister-4b944.appspot.com",
+    messagingSenderId: "873374776677",
+    appId: "1:873374776677:web:fba4eec6d6507d0af4758c"
   };
+
   // Initialize Firebase
   firebase.initializeApp(config);
 
